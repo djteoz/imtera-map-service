144cd8825a1fd696ea5b9c630314c7362fcd2484
djteoz <djteozfrance@yandex.ru>
2026-02-12 19:33:07 +0300

    chore: add user seeder (admin@example.com / password)

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password')
+            ]
+        );
+    }
+}
