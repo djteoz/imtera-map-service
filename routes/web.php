7e4436c93ecff629dd68703a36c8fc41ffd72cce
djteoz <djteozfrance@yandex.ru>
2026-02-14 00:20:29 +0300

    Add web routes and copy HTML files to public

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,14 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { return view('app'); });
+Route::get('/', function () {
+    return redirect('/demo.html');
+});
+
+Route::get('/demo.html', function () {
+    return response()->file(public_path('demo.html'));
+});
+
+Route::get('/index.html', function () {
+    return response()->file(public_path('index.html'));
+});
