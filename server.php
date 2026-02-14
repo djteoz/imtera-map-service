dfcb342a971829f8ccd2e7fc62ec84d6913098df
djteoz <djteozfrance@yandex.ru>
2026-02-14 10:15:31 +0300

    Fix missing base Controller and serve root demo statically

--- server.php
+++ server.php
@@ -4,6 +4,11 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
 
+if ($uri === '/') {
+    readfile(__DIR__.'/public/demo.html');
+    return true;
+}
+
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
